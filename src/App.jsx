da1acca8c8a81b392231970b6495f304e33ec80e
rupam905 <rupamdutta905@gmail.com>
2025-02-28 10:21:06 +0530

    added further ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { RainbowButton } from './components/magicui/rainbow-button'
+// import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const resetcounter = ()=> {
+    setCount(0);
+  }
 
   return (
     <>
@@ -18,9 +23,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button  onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button >
+        &emsp;
+        <button onClick={resetcounter}>
+          Reset
         </button>
+        <RainbowButton></RainbowButton>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
